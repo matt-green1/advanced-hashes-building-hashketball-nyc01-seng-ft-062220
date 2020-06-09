43c3f1b0dba57711e76a493304427c9bff51e79a
Matt Green <magreen118@gmail.com>
2020-06-09 00:39:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,12 +3,24 @@ nds = {
   home: {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
-    :players => []
+    :players => [
+      {},
+      {},
+      {},
+      {},
+      {}
+      ]
   },
   away: {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
-    :players => []
+    :players => [
+      {},
+      {},
+      {},
+      {},
+      {}
+      ]
   }
 }  
 return nds  
