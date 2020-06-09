b3683d76af749ffd9eddfe1dbf4aa2878e993c8d
Matt Green <magreen118@gmail.com>
2020-06-09 00:43:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,15 +5,15 @@ nds = {
     :colors => ["Black", "White"],
     :players => [
       {
-        :player_name
-        :number
-        :shoe
-        :points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks
+        :player_name =>
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
       },
       {},
       {},
